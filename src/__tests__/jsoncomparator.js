2b113ac86e044dfe8cb6e0d47bad992568353dc7
kalynov <temachempion@gmail.com>
2023-12-12 16:32:13 +0100

    step 4: add actionns and tests

---
+++ src/__tests__/jsoncomparator.js
@@ -0,0 +1,29 @@
+import { comparator } from '../utils.js';
+
+const obj1 = {
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+  follow: false,
+};
+
+const obj2 = {
+  timeout: 20,
+  verbose: true,
+  host: 'hexlet.io',
+};
+
+const result = [
+  ' - follow: false',
+  '   host: hexlet.io',
+  ' - proxy: 123.234.53.22',
+  ' - timeout: 50',
+  ' + timeout: 20',
+  ' + verbose: true',
+];
+
+describe('Is channel Level test', () => {
+  test('flow id', () => {
+    expect(comparator(obj1, obj2)).toEqual(result);
+  });
+});
