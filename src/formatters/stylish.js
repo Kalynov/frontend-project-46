a8ea41af7ef0813f194a5bef8dc0b19c33a8a656
kalynov <temachempion@gmail.com>
2024-06-12 20:41:44 +0200

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,36 +8,38 @@ const signs = {
   [WITHOUTCHANGE]: '   ',
 };
 
-const getMargin = (deep = 0) => new Array(deep * 2).fill('   ').join('');
+const getMargin = (deep = 1, spacesCount = 4) => ' '.repeat(deep * spacesCount - 2);
 
 const stringify = (value, deep) => {
   if (value instanceof Object) {
-    return `{\n${Object.keys(value).map((key) => `   ${getMargin(deep + 1)}   ${key}: ${stringify(value[key], deep + 1)}\n`).join('')}   ${getMargin(deep)}   }`;
+    return `{\n${Object.keys(value).map((key) => `${getMargin(deep + 1)}${key}: ${stringify(value[key], deep + 1)}\n`).join('')}${getMargin(deep)}}`;
   }
   // console.log(String(value));
   return String(value);
 };
 
-const stylish = (differences, deep = 0) => {
+const stylish = (differences, deep = 1) => {
   const result = differences.map((dif) => {
     if (dif.childrens) {
       if (dif.state === CHANGED) {
-        return `${getMargin(deep)}   ${signs[REMOVED]}${dif.key}: ${stylish(dif.childrens, deep + 1)}
-${getMargin(deep)}   ${signs[ADDED]}${dif.key}: ${stylish(dif.childrens, deep + 1)}`;
+        return `${getMargin(deep)}${signs[REMOVED]}${dif.key}: ${stylish(dif.childrens, deep + 1)}
+${getMargin(deep)}${signs[ADDED]}${dif.key}: ${stylish(dif.childrens, deep + 1)}`;
       }
-      return `${getMargin(deep)}   ${signs[dif.state]}${dif.key}: ${stylish(dif.childrens, deep + 1)}`;
+      return `${getMargin(deep)}${signs[dif.state]}${dif.key}: ${stylish(dif.childrens, deep + 1)}`;
     }
     switch (dif.state) {
       case ADDED: {
-        return `${getMargin(deep)}   ${signs[dif.state]}${dif.key}: ${stringify(dif.value1, deep)}`;
+        return `${getMargin(deep)}${signs[dif.state]}${dif.key}: ${stringify(dif.value1, deep)}`;
       }
       case REMOVED:
       case WITHOUTCHANGE: {
-        return `${getMargin(deep)}   ${signs[dif.state]}${dif.key}: ${stringify(dif.value0, deep)}`;
+        return deep !== 1
+          ? `${getMargin(deep)}${signs[dif.state]}${dif.key}: ${stringify(dif.value0, deep)}`
+          : `${getMargin(deep)}${dif.key}: ${stringify(dif.value0, deep)}`;
       }
       case CHANGED: {
-        return `${getMargin(deep)}   ${signs[REMOVED]}${dif.key}: ${stringify(dif.value0, deep)}
-${getMargin(deep)}   ${signs[ADDED]}${dif.key}: ${stringify(dif.value1, deep)}`;
+        return `${getMargin(deep)}${signs[REMOVED]}${dif.key}: ${stringify(dif.value0, deep)}
+${getMargin(deep)}${signs[ADDED]}${dif.key}: ${stringify(dif.value1, deep)}`;
       }
       default:
         return '';
