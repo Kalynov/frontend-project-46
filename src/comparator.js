d2f0f72d1cf1d9884a4ec2958c5d5c54f68ff1e0
kalynov <temachempion@gmail.com>
2024-06-26 21:17:30 +0200

    fix code smells

--- src/comparator.js
+++ src/comparator.js
@@ -26,131 +26,45 @@ const compareSorter = (a, b) => {
   return 0;
 };
 
-// const comparator = (obj1, obj2) => {
-//   const result = [];
-//   const obj1Keys = Object.keys(obj1);
-//   const obj2keys = Object.keys(obj2);
-//   obj1Keys.forEach((key) => {
-//     result.push({
-//       name: key,
-//       type: obj1[key] instanceof Object ? OBJ : PRIMITIVE,
-//       state: REMOVED,
-//       value1: obj1[key],
-//       childrens: obj1[key] instanceof Object ? comparator(obj1[key], obj1[key]) : undefined,
-//     });
-//   });
+const threeBuildCb = (obj1, obj2, key) => {
+  if (!Object.hasOwn(obj1, key)) {
+    return ({
+      key,
+      state: ADDED,
+      value1: obj2[key],
+    });
+  }
+  if (!Object.hasOwn(obj2, key)) {
+    return ({
+      key,
+      state: REMOVED,
+      value0: obj1[key],
+    });
+  }
+  if ((!(obj2[key] instanceof Object) && obj1[key] instanceof Object)
+    || (obj2[key] instanceof Object && !(obj1[key] instanceof Object))
+    || (obj1[key] !== obj2[key])) {
+    return ({
+      key,
+      state: CHANGED,
+      value0: obj1[key],
+      value1: obj2[key],
+    });
+  }
+  return ({
+    key,
+    state: WITHOUTCHANGE,
+    value0: obj2[key],
+    value1: obj2[key],
+    childrens: (obj2[key] instanceof Object && obj1[key] instanceof Object) ? comparator(obj1[key], obj2[key]) : undefined,
+  });
+};
 
-//   obj2keys.forEach((key) => {
-//     const keyFromResult = result.find((el) => el.name === key);
-//     if (keyFromResult?.type === OBJ) {
-//       if (obj2[key] instanceof Object) {
-//         keyFromResult.state = WITHOUTCHANGE;
-//         keyFromResult.childrens = comparator(obj1[key], obj2[key]);
-//       }
-//       if (!(obj2[key] instanceof Object)) {
-//         keyFromResult.state = CHANGED;
-//         result.push({
-//           name: key,
-//           value: obj2[key],
-//           state: ADDED,
-//         });
-//       }
-//     } else if (keyFromResult?.type === PRIMITIVE) {
-//       if (obj2[key] instanceof Object) {
-//         keyFromResult.state = REMOVED;
-//         result.push({
-//           name: key,
-//           state: ADDED,
-//           value: obj2[key],
-//           childrens: comparator({}, obj2[key]),
-//         });
-//       } else if (keyFromResult.value !== obj2[key]) {
-//         keyFromResult.state = REMOVED;
-//         result.push({
-//           name: key,
-//           state: ADDED,
-//           value: obj2[key],
-//         });
-//       } else {
-//         keyFromResult.state = WITHOUTCHANGE;
-//       }
-//     } else {
-//       result.push({
-//         name: key,
-//         state: ADDED,
-//         value: obj2[key],
-//         childrens: obj2[key] instanceof Object ? comparator(obj2[key], obj2[key]) : undefined,
-//       });
-//     }
-//   });
-//   return result.sort(compareSorter);
-// };
 
 const comparator = (obj1, obj2) => {
   const keys = new Set(Object.keys(obj1).concat(Object.keys(obj2)));
-
-  const compareThree = Array.from(keys).map((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      return ({
-        key,
-        state: ADDED,
-        value1: obj2[key],
-      });
-    }
-    if (!Object.hasOwn(obj2, key)) {
-      return ({
-        key,
-        state: REMOVED,
-        value0: obj1[key],
-      });
-    }
-
-    if (obj2[key] instanceof Object && obj1[key] instanceof Object) {
-      return ({
-        key,
-        state: WITHOUTCHANGE,
-        value0: obj1[key],
-        value1: obj2[key],
-        childrens: comparator(obj1[key], obj2[key]),
-      });
-    }
-
-    if (!(obj2[key] instanceof Object) && obj1[key] instanceof Object) {
-      return ({
-        key,
-        state: CHANGED,
-        value0: obj1[key],
-        value1: obj2[key],
-        // childrens: comparator(obj1[key], obj1[key]),
-      });
-    }
-
-    if (obj2[key] instanceof Object && !(obj1[key] instanceof Object)) {
-      return ({
-        key,
-        state: CHANGED,
-        value0: obj1[key],
-        value1: obj2[key],
-        // childrens: comparator(obj2[key], obj2[key]),
-      });
-    }
-
-    if (obj1[key] !== obj2[key]) {
-      return ({
-        key,
-        state: CHANGED,
-        value0: obj1[key],
-        value1: obj2[key],
-      });
-    }
-
-    return ({
-      key,
-      state: WITHOUTCHANGE,
-      value0: obj2[key],
-      value1: obj2[key],
-    });
-  });
+  const compareThree = Array.from(keys).map((key) => threeBuildCb(obj1, obj2, key));
+  console.log(compareThree);
   return compareThree.toSorted(compareSorter);
 };
 
