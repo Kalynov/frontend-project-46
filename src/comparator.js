a358cfff32ef9f2c1b5c5916f850caa4bd8946c8
kalynov <temachempion@gmail.com>
2024-06-26 21:45:14 +0200

    fix comparator

--- src/comparator.js
+++ src/comparator.js
@@ -26,42 +26,60 @@ const compareSorter = (a, b) => {
   return 0;
 };
 
+const getStatus = (obj1, obj2, key) => {
+  if (!Object.hasOwn(obj1, key)) {
+    return ADDED
+  }
+  if (!Object.hasOwn(obj2, key)) {
+    return REMOVED
+  }
+  if (obj2[key] instanceof Object && obj1[key] instanceof Object) {
+    return WITHOUTCHANGE
+  }
+  if ((!(obj2[key] instanceof Object) && obj1[key] instanceof Object)
+    || (obj2[key] instanceof Object && !(obj1[key] instanceof Object))
+    || (obj1[key] !== obj2[key])) {
+    return CHANGED
+  }
+  return WITHOUTCHANGE;
+}
+
 const comparator = (obj1, obj2) => {
   const keys = new Set(Object.keys(obj1).concat(Object.keys(obj2)));
   const compareThree = Array.from(keys).map((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      return ({
-        key,
-        state: ADDED,
-        value1: obj2[key],
-      });
+    const status = getStatus(obj1, obj2, key);
+    switch (status) {
+      case ADDED:
+        return ({
+          key,
+          state: status,
+          value1: obj2[key],
+        });
+      case REMOVED:
+        return ({
+          key,
+          state: status,
+          value0: obj1[key],
+        });
+      case CHANGED:
+        return ({
+          key,
+          state: status,
+          value0: obj1[key],
+          value1: obj2[key],
+        });
+      case WITHOUTCHANGE:
+      default: 
+        return ({
+          key,
+          state: WITHOUTCHANGE,
+          value0: obj1[key] ? obj1[key] : obj2[key],
+          value1: obj2[key],
+          childrens: (obj2[key] instanceof Object && obj1[key] instanceof Object)
+            ? comparator(obj1[key], obj2[key])
+            : undefined,
+        });
     }
-    if (!Object.hasOwn(obj2, key)) {
-      return ({
-        key,
-        state: REMOVED,
-        value0: obj1[key],
-      });
-    }
-    if ((!(obj2[key] instanceof Object) && obj1[key] instanceof Object)
-      || (obj2[key] instanceof Object && !(obj1[key] instanceof Object))
-      || (obj1[key] !== obj2[key])) {
-      return ({
-        key,
-        state: CHANGED,
-        value0: obj1[key],
-        value1: obj2[key],
-      });
-    }
-    return ({
-      key,
-      state: WITHOUTCHANGE,
-      value0: obj2[key],
-      value1: obj2[key],
-      childrens: (obj2[key] instanceof Object && obj1[key] instanceof Object)
-        ? comparator(obj1[key], obj2[key])
-        : undefined,
-    });
   });
   return compareThree.toSorted(compareSorter);
 };
